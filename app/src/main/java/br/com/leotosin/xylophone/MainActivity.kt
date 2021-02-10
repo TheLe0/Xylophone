b5e218eeed3a01859d13b6c557f2fbbdb81bf107
TheLe0 <leob.tosin@hotmail.com>
2021-02-09 21:35:12 -0300

    Create the layout

--- app/src/main/java/br/com/leotosin/xylophone/MainActivity.kt
+++ app/src/main/java/br/com/leotosin/xylophone/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.leotosin.xylophone
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun playSound()
+    {
+        val sound :MediaPlayer = MediaPlayer.create(this, R.raw.note1)
+
+        sound.start()
     }
 }
